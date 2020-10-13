9c1b581bb6e14e01b854f429d73b75180b70e901
Alquamah Shahid <alqamahjsr01@gmail.com>
2020-10-13 14:01:15 +0530

    Place Order Functionality Added

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,9 +1,9 @@
 import asyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
 
-//@desc     Create new Order
-//@route    POST /api/orders
-//@access   Private
+// @desc    Create new order
+// @route   POST /api/orders
+// @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
   const {
     orderItems,
@@ -30,7 +30,9 @@ const addOrderItems = asyncHandler(async (req, res) => {
       shippingPrice,
       totalPrice,
     });
+
     const createdOrder = await order.save();
+
     res.status(201).json(createdOrder);
   }
 });
